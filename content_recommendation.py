328fa6241af48de2fb37adba4fc25d217d745cd8
Chinthaka <chinthaka.jayarathne@axiatadigitallabs.com>
2021-08-22 12:43:13 +0530

    Reformat code for readability

--- content_recommendation.py
+++ content_recommendation.py
@@ -1,7 +1,4 @@
-
-import seaborn as sns
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 def recommendation():
@@ -15,30 +12,29 @@ def recommendation():
     # system get the gender from API
     new = hello[hello['Gender'] == 'input_gender']
 
-    #################code start
+    # code start
 
     # grouping the items count and the order rate in the data set in the ascending order
     new.groupby('ItemName')['order rate'].mean().sort_values(ascending=False).head()
 
     new.groupby('ItemName')['order rate'].count().sort_values(ascending=False).head()
 
-
-    #take the mean of the above grouping
+    # take the mean of the above grouping
     Order_rating = pd.DataFrame(new.groupby('ItemName')['order rate'].mean())
-    ##print(Order_rating.head())
+    # print(Order_rating.head())
 
     # how no of order_rate has flowed with mean with order_rate (ItemName,order rate)
     # here the order rate depicts the mean of the order rate
     # here the num of Order_rate depicts the actual no of order rate
     Order_rating['num of Order_rate'] = pd.DataFrame(new.groupby('ItemName')['order rate'].count())
-    ##print(Order_rating.head())
+    # print(Order_rating.head())
 
     # CustomerID,ItemName,order rate distribution in dataset using table
     itemmat = new.pivot_table(index='CustomerID', columns='ItemName', values='order rate')
-    ##print(itemmat.head())
+    # print(itemmat.head())
 
     # sort num of Order_rate in the ascending order
-    Order_rating.sort_values('num of Order_rate',ascending=False).head(10)
+    Order_rating.sort_values('num of Order_rate', ascending=False).head(10)
 
     Order_rating.head(20)
 
@@ -51,13 +47,13 @@ def recommendation():
 
     corr_x = pd.DataFrame(similar_to_x, columns=['Correlation'])
     corr_x.dropna(inplace=True)
-    ##print(corr_x.head())
+    # print(corr_x.head())
 
     corr_xx = corr_x.join(Order_rating['num of Order_rate'])
-    ##print(corr_xx)
+    # print(corr_xx)
 
     recom_data = corr_xx[corr_xx['num of Order_rate'] > 4].sort_values('Correlation', ascending=False)
-    ##print(recom_data)
+    # print(recom_data)
 
     output = recom_data['Correlation']
     output.head(5)
@@ -70,8 +66,3 @@ class Content:
         self.input_item = input_item
         self.input_age = input_age
         self.input_gender = input_gender
-
-
-
-
-
--- server.py
+++ server.py
@@ -6,16 +6,17 @@ from content_recommendation import Content, recommendation
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/item_recommendation', methods=['POST'])
-
 def recommend_content():
-        instance = request.get_json(silent=True)
-        text1 = instance["text1"]
-        text2 = instance["text2"]
-        text3 = instance["text3"]
-        content = Content(text1, text2, text3)
+    instance = request.get_json(silent=True)
+    text1 = instance["text1"]
+    text2 = instance["text2"]
+    text3 = instance["text3"]
+    content = Content(text1, text2, text3)
 
-        return recommendation()
+    return recommendation()
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
