cb18de727d13aa30d68f06a4bb65293fa660e5cb
Chinthaka <chinthaka.jayarathne@axiatadigitallabs.com>
2021-08-22 14:31:32 +0530

    Add logger to server.py

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask, request
 from flask_cors import CORS
 from flask_expects_json import expects_json
 
+from util.config_loader import config
+from util.logger import get_logger
+
+logger = get_logger("main")
+
 app = Flask(__name__)
 CORS(app)
 
@@ -20,10 +25,11 @@ schema = {
 @expects_json(schema)
 def recommend_content():
     data = request.get_json()
-    if "age" in data:
+    # if "age" in data:
 
     return "Success"
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    logger.info("Starting application using profile: %s", config['PROFILE']['name'])
+    app.run(host=config['SERVER']['host'], port=int(config['SERVER']['port']), debug=True)
